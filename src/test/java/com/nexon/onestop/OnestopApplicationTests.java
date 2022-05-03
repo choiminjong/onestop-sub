903b8490c1e90cdb26ebdde50570519ef0e31fe1
민종 최 <dominic@nexon.co.kr>
2022-05-03 11:14:02 +0900

    init

---
+++ src/test/java/com/nexon/onestop/OnestopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexon.onestop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnestopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
