8dde572a4d19f749df91b8d10439d5b44697ddd7
민종 최 <dominic@nexon.co.kr>
2022-05-10 08:35:51 +0900

    @EntityGraph(attributePaths = { "roleSet" }) 설정 후 쿼리 검토 완료

--- src/main/java/com/nexon/onestop/repository/ResourcesRepository.java
+++ src/main/java/com/nexon/onestop/repository/ResourcesRepository.java
@@ -4,6 +4,7 @@ import com.nexon.onestop.domain.entity.Resources;
 import com.nexon.onestop.domain.entity.Role;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,9 +12,11 @@ import java.util.List;
 
 public interface ResourcesRepository extends JpaRepository<Resources, Long> {
 
+    @EntityGraph(attributePaths = { "roleSet" })
     @Query("select r from Resources r join fetch r.roleSet where r.resourceType = 'url' order by r.orderNum desc")
     List<Resources> findAllResources();
 
+    @EntityGraph(attributePaths = { "roleSet" })
     @Query("select r from Resources r where r.resourceType = 'url' order by r.orderNum asc")
     Page<Resources> findPageResources(Pageable pageable);
 
